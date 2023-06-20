feced1fe9e728ad5a99894671c7b033d9a9f1488
Bogdan Sebastian Blanaru <bogdan_blanaru@epam.com>
2023-06-20 10:47:56 +0300

    Blog-page: fix

--- src/app/public/services/register-service.service.ts
+++ src/app/public/services/register-service.service.ts
@@ -1,16 +1,15 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { registerData } from '../interfaces/registerInterface'
-import { baseURLDatabase } from 'src/environoments/environoment'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { registerData } from '../../models/interfaces/registerInterface';
+import { baseURLDatabase } from 'src/environoments/environoment';
 
-@Injectable( {
-  providedIn: 'root'
-} )
+@Injectable({
+  providedIn: 'root',
+})
 export class RegisterServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor ( private http: HttpClient ) {}
-
-  register( data: registerData ) {
-    alert( 'Account registartion completed succefully' )
+  register(data: registerData) {
+    alert('Account registartion completed succefully');
   }
 }
